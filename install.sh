62ad7f4bf0c1bfdf2eb2b6fae517645fe5a7ffd8
Bhaallord <Bhaallord@arcor.de>
2011-07-13 10:00:03 +0000

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pdflatex formel.tex -halt-on-error  
-#pdflatex formel.tex
-#pdflatex formel.tex
+pdflatex formel.tex  
+pdflatex formel.tex
+pdflatex formel.tex
 rm *.aux 
 rm *.log
 rm *.toc
